ad9e0075c6c631f3a07fd77163e0783c9e4d2d22
Rohit Ahuja <hackerrohit101@gmail.com>
2018-06-10 23:40:06 +0530

    visualization

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -37,7 +37,25 @@ lin_reg.fit(X,Y)
 
 #Fitting Polynomial Regression to dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 5)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly,Y)
+
+#Visualising the Linear Regression result
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg.predict(X),color = 'blue')
+plt.title('Truth or Bluff(Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Polynomial result
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,Y,color = 'red')
+plt.plot(X_grid,lin_reg_2.predict(poly_reg.fit_transform(X_grid)),color = 'blue')
+plt.title('Truth or Bluff(Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
